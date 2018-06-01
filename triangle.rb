6a441d5e0fe639247fde3471d0d40570c9e362fc
zap-rowsdower <brock.whitbread@gmail.com>
2018-06-01 16:04:48 -0700

    passed.

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,31 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # FIRST TRY
+  # if a != b && a != c && b == c
+  #   :isosceles
+  # elsif a != b && b != c && a == c
+  #   :isosceles
+  # elsif a != c && b != c && a == b
+  #   :isosceles
+  # elsif a == b && a == c
+  #   :equilateral
+  # elsif a != b && a != c
+  #   :scalene
+  # end
+
+
+  if (a <= 0) || (b <= 0) || (c <= 0)
+    raise TriangleError, "a side can not be equal to 0 or a negative number"
+  elsif (a+b <= c) || (a+c <= b) || (b+c <= a)
+    raise TriangleError, "the sum of two sides can not be greater than the third"
+  elsif a == b && b == c then
+    :equilateral
+  elsif a == b || a == c || b == c then
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
